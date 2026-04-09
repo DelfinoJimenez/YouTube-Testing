d5bcbda0b44261537b83f09d38c079cd6eb90c58
Lennard <165533377+LennardBecker@users.noreply.github.com>
2026-04-08 23:09:12 -0400

    Update Comment.java

--- Comment.java
+++ Comment.java
@@ -1,22 +1,16 @@
-package org.example;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
-import java.util.List;
 
 public class Comment {
     WebDriver driver;
